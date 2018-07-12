ee9f09d1cb983e1dc208b7196e3d4b6c3de307b1
Kar <krlosbatdev@gmail.com>
2018-07-11 21:56:41 -0400

    Add mapping profile

---
+++ ideafinancial/Mapping/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using ideafinancial.Controllers.Resources;
+using ideafinancial.Models;
+
+namespace ideafinancial.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserResource>();
+        }
+    }
+}
